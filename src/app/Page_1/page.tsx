38f36a11f60c102d31744fe0ebecaf63967ae81b
Beqa <tvildiani2001@gmail.com>
2025-07-27 16:24:38 +0200

    updates

--- src/app/Page_1/page.tsx
+++ src/app/Page_1/page.tsx
@@ -1,10 +1,26 @@
+"use client";
+
+import { useLocalStorage } from "@/components/shared/useLocalStorage";
+import { useEffect, useState } from "react";
+
 export default function Home() {
+    const [language] = useLocalStorage("language");
+    const headText = language === "en" ? "Page 1 Template" : "გვერდი 1 თემა";
+    const buttonText = language === "en" ? "HELLO BUTTON" : "გამარჯობა ღილაკი";
+    const [isHydrated, setIsHydrated] = useState(false);
+
+    useEffect(() => {
+        setIsHydrated(true);
+    }, []);
+
+    if (!isHydrated) return null;
+
     return (
         <div className="min-h-screen flex flex-col justify-center items-center">
             <main className="flex flex-col justify-center items-center">
-                <h1 className="text-4xl font-bold mb-4">Page 1 Template</h1>
+                <h1 className="text-4xl font-bold mb-4">{headText}</h1>
                 <button className="text-lg shadow__btn large scrolly bg-purple-600 hover:bg-purple-700 font-black py-3 px-6 rounded-lg hover:scale-105 duration-300">
-                    HELLO from Page 1
+                    {buttonText}
                 </button>
             </main>
         </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { ReactNode } from "react";
 import { ToastContainer } from "react-toastify";
+import Navbar from "@/components/Navbar";
 
 export const metadata = {
     title: "Template",
@@ -13,33 +14,10 @@ interface AppProps {
 
 export default function App({ children }: AppProps) {
     return (
-        <html lang="en">
+        <html lang={"en"}>
             <body>
-                {/* Shared Navbar (if needed) */}
-                <nav className="bg-gray-800 p-4">
-                    <ul className="container mx-auto flex gap-1 items-center">
-                        <li>
-                            <a href="/" className="text-white duration-300 hover:bg-gray-700 p-3 rounded-lg">
-                                Home
-                            </a>
-                        </li>
-                        <li>
-                            <a href="Page_1" className="text-white duration-300 hover:bg-gray-700 p-3 rounded-lg">
-                                Page 1
-                            </a>
-                        </li>
-                        <li>
-                            <a href="Page_2" className="text-white duration-300 hover:bg-gray-700 p-3 rounded-lg">
-                                Page 2
-                            </a>
-                        </li>
-                        <li>
-                            <a href="Page_3" className="text-white duration-300 hover:bg-gray-700 p-3 rounded-lg">
-                                Page 3
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
+                {/* Navbar */}
+                <Navbar />
 
                 {/* Main Content */}
                 <main className="bg-gray-900 min-h-[85vh]">{children}</main>
